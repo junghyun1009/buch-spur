14d2eb12aa53c9b23dc83283b2d4cbc57c3e5516
JungHyun <junghyunk1009@gmail.com>
2025-07-29 15:48:32 +0900

    feat(books): :sparkles: Add APIView and Import
    Add BookListAPIView in views.py and Import in urls.py

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import BookListAPIView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', BookListAPIView.as_view()),
 ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+class BookListAPIView(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
