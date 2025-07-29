a379d81586c007f75121b6c13e9cb196976bfbe1
JungHyun <junghyunk1009@gmail.com>
2025-07-29 16:55:45 +0900

    chore(project): :sparkles: Add CORS to synchronize with Vue
    Install django-cors-headers and Add settings in settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'books',
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'myproject.urls'
@@ -122,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Vue와 연동하기 위한 CORS 설정
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
