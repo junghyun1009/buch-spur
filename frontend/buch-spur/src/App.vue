fb523359a6166c89147ca9135fbb7657d7f40984
JungHyun <junghyunk1009@gmail.com>
2025-07-30 17:33:36 +0900

    feat(users): :sparkles: Add signup form
    Add signup form, Synchronize with django

--- frontend/buch-spur/src/App.vue
+++ frontend/buch-spur/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <BookList />
+    <SignupFrom />
   </div>
 </template>
 
 <script>
 import BookList from './components/BookList.vue'
+import SignupForm from './components/user/SignupForm.vue'
 
 export default {
   name: 'App',
   components: {
-    BookList
+    BookList,
+    SignupForm
   }
 }
 </script>
---
+++ frontend/buch-spur/src/components/user/SignupForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <form @submit.prevent="handleSignup">
+            <input v-model="username" placeholder="아이디">
+            <input v-model="password" placeholder="비밀번호">
+            <input v-model="email" placeholder="이메일">
+            <input v-model="nickname" placeholder="닉네임">
+            <button type="submit">회원가입</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import axios from 'axios'
+
+const username = ref('')
+const password = ref('')
+const email = ref('')
+const nickname = ref('')
+
+const handleSignup = async () =>{
+    try {
+        const response = await axios.post('/signup/', {
+            username: username.value,
+            password: password.value,
+            email: email.value,
+            nickname: nickname.value
+        })
+        alert('회원가입이 완료되었습니다.')
+    } catch (error) {
+        alert('회원가입을 실패하였습니다.')
+    }
+}
+
+</script>
