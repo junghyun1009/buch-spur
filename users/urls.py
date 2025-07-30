6b63c25a378956cb99be824aa254614c8af902e1
JungHyun <junghyunk1009@gmail.com>
2025-07-30 16:35:59 +0900

    feat(users): :sparkles: Add urls for signup, login, logout
    Add signup, login, logout urls in urls.py

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import SignupAPIView, LoginAPIView, LogoutAPIView
+
+urlpatterns = [
+    path('signup/', SignupAPIView.as_view(), name='signup'),
+    path('login/', LoginAPIView.as_view(), name='login'),
+    path('logout/', LogoutAPIView.as_view(), name='logout'),
+]
