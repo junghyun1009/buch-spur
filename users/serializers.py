60f617ffeccc99d99095c7bfcb9c68bb55d0a48a
JungHyun <junghyunk1009@gmail.com>
2025-07-30 16:02:25 +0900

    feat(users): :sparkles: Add serializer for signup
    Add UserSignupSerializer in serializers.py

---
+++ users/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fieds = ('username', 'password', 'email', 'nickname')
+
+    def create(self, validated_data):
+        user = User(
+            username = validated_data['username'],
+            email = validated_data.get('email'),
+            nickname = validated_data.get('nickname')
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
